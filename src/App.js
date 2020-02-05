9e3e2ad7063dfc709e4929d639c7ca7860501d87
root <pimenoff.niklas@gmail.com>
2020-02-05 13:38:04 +0000

    fixed handleClick to not do anything if package does not exist on system. Could add an alert later on

--- src/App.js
+++ src/App.js
@@ -6,25 +6,23 @@ import dpkg_service from "./services/dpkg"
 const App = (props) => {
 	const [packages, setPackages] = useState([])
 	const [pack, setPack] = useState(null)
-	// const test = React.createRef()
 	useEffect(() => {
 		dpkg_service.getAll().then(packages => setPackages(packages))
 	}, [])
 	const handleClick = async (name) => {
 		const a = await packages.find(pack => pack.Package === name)
+		if (!a)
+			return
 		setPack(a)
 	}
-	console.log(pack)
 	return (
 		<div>
 			<h2>Packages</h2>
-			<div className="container">
 				<Packages packages={packages} event={handleClick}/>
 				{pack === null ?
 					null :
-					<Package_info pack={pack}/>
+					<Package_info pack={pack} event={handleClick}/>
 				}
-			</div>
 		</div>
 	)
 }
